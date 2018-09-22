069204dc8f40a9aff7e43f2486964eec21ab319d
kozy10 <kentaro1002vb@gmail.com>
2018-09-22 13:46:57 +0000

    Add notification by action mailer and update layouts of manages and practices

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+
+	def breadcrumb(crumbs)
+    return if crumbs.nil?
+    content_tag :ol, class: 'breadcrumb' do
+      crumbs.each_with_index do |crumb, i|
+        if i == crumbs.size - 1
+          concat (content_tag :li, crumb[:title], class: 'breadcrumb-item active')
+        else
+          concat (content_tag :li, (link_to crumb[:title], crumb[:path]), class: 'breadcrumb-item')
+        end
+      end
+    end
+  end
+
   def active_controller_and_action(controller_name, action_name)
     if params[:controller] == controller_name && params[:action] ==  action_name
       "active"
@@ -15,7 +29,5 @@ module ApplicationHelper
 		action.include?(params[:action])#引数が現在のアクションに含まれていたらtrueを返す
 	end
 
-	def hoge
-		"hello"
-	end
+	
 end

c8c8497a2f7a429970f6353235879f2f46760b04
kozy10 <kentaro1002vb@gmail.com>
2018-10-04 12:01:40 +0000

    Change config/environment/production.rb config.eager_load to false

--- app/controllers/user/top_controller.rb
+++ app/controllers/user/top_controller.rb
@@ -1,7 +1,7 @@
 class User::TopController < ApplicationController
 	layout 'user'
 	def index
-		geolocation = [35.8077379, 139.7241712]
+		geolocation = [10.8077379, 139.7241712]
 		@teams = Team.near(geolocation, 3, order: 'distance').limit(4)
 		render 'user/top'
 	end

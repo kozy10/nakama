32fb0e4b882220ab2b3fcada97421ef76a2b85a1
kozy10 <kentaro1002vb@gmail.com>
2018-10-19 11:23:14 +0000

    Add slide show using lightbox

--- app/assets/javascripts/user/user.js
+++ app/assets/javascripts/user/user.js
@@ -1,5 +1,4 @@
 //= require jquery
-
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require dropzone
@@ -7,6 +6,7 @@
 //= require toastr
 //= require turbolinks
 //= require refile
+//= require lightbox
 //= require_tree .
 
 /*global toastr*/
